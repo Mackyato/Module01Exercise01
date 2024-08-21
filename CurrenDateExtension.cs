10f8da326befa69ebfb97b3ba633ff1a646c9e3e
Jansen Cruz <jansencruz12345@gmail.com>
2024-08-21 13:49:38 +0800

    Add project files.

---
+++ CurrenDateExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Exercise01
+{
+    public class CurrenDateExtension : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"Welcome to my application the date today is {DateTime.Now.ToString("D")}";
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorRed { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello!!";
+            ColorRed = Colors.Black;
+
+            this.BindingContext = this;
+
+        }
+
+        //DatePicker birthDatePicker = new DatePicker()
+        //{
+           
+        //};
+
+        private void OnChangedBackgroundColorG(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Aqua;
+        }
+        private void OnChangedBackgroundColorL(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var name = MyName.Text;
+            var email = Email.Text;
+        }
+
+    }
+
+}
